8407117e3109ea1015e7e9401efc1d0daec8c8ea
David Dias <davidcostadias@gmail.com>
2025-02-25 16:59:21 -0300

    FileCalc final

---
+++ FileCalc/FileCalc/Entities/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCalc.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalValue()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + TotalValue().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ FileCalc/FileCalc/Program.cs
@@ -0,0 +1,47 @@
+﻿using FileCalc.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+
+namespace FileCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = "C:\\Users\\ddvil\\Documents\\DEV\\repos\\treino-cli\\FileCalc\\file.csv";
+            string targetPath = "C:\\Users\\ddvil\\Documents\\DEV\\repos\\treino-cli\\FileCalc\\out\\summary.csv";
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                string[] aux = File.ReadAllLines(sourcePath);
+
+                for (int i = 0; i < aux.Length; i++)
+                {
+                    string[] prod = aux[i].Split(',');
+                    string name = prod[0];
+                    double price = double.Parse(prod[1], CultureInfo.InvariantCulture);
+                    int quantity = int.Parse(prod[2]);
+                    Product product = new Product(name, price, quantity);
+                    products.Add(product);
+                }
+
+                using (StreamWriter sw = new StreamWriter(targetPath))
+                {
+                    foreach (Product prod in products)
+                    {
+                        sw.WriteLine(prod);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
