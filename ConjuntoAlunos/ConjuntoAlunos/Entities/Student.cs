5a704c5db147c0b337d2b680650b0cbc3e5aa09c
David Dias <davidcostadias@gmail.com>
2025-02-28 11:02:03 -0300

    ConjuntoAlunos inicial

---
+++ ConjuntoAlunos/ConjuntoAlunos/Entities/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace ConjuntoAlunos.Entities
+{
+    struct Student
+    {
+        public int Code { get; set; }
+
+        public Student(int code) : this()
+        {
+            Code = code;
+        }
+    }
+}
---
+++ ConjuntoAlunos/ConjuntoAlunos/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConjuntoAlunos.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ConjuntoAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> studentList = new HashSet<Student>();
+            Console.Write("How many students for course A? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                studentList.Add(new Student(id));
+            }
+
+            Console.Write("How many students for course B? ");
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                studentList.Add(new Student(id));
+            }
+
+            Console.Write("How many students for course C? ");
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                studentList.Add(new Student(id));
+            }
+
+            Console.Write("Total students: ");
+            Console.WriteLine(studentList.Count);
+
+        }
+    }
+}
