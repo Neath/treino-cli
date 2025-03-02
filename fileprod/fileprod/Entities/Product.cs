99a53121e49c508c80f837b61237ef1e85148abf
David Dias <davidcostadias@gmail.com>
2025-03-02 12:51:37 -0300

    fileprod inicial

---
+++ fileprod/fileprod/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace fileprod.Entities
+{
+    class Product
+    {
+        List<Product> list = new List<Product>();
+
+        public Product()
+        {
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void AddProcut(Product prod)
+        {
+            list.Add(prod);
+        }
+    }
+}
---
+++ fileprod/fileprod/Program.cs
@@ -0,0 +1,49 @@
+﻿using fileprod.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace fileprod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product;
+            List<Product> list = new List<Product>();
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] aux = sr.ReadLine().Split(',');
+                        string name = aux[0];
+                        double price = double.Parse(aux[1], CultureInfo.InvariantCulture);
+                        product = new Product(name, price);
+                        list.Add(product);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            double average = list.Average(p => p.Price);
+            Console.WriteLine("Average price: " + average.ToString("C2"));
+
+            var selected = list.Where(p => p.Price < average).OrderByDescending(p => p.Name);
+            foreach (Product p in selected)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+        }
+    }
+}
